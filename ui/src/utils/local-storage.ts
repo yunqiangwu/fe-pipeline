1192f6acf192a892dcb3544cb718954543eee7c9
yunqiangwu <842269153@qq.com>
2021-04-13 16:42:53 +0800

    fix: 报错发生消息给父应用

--- ui/src/utils/local-storage.ts
+++ ui/src/utils/local-storage.ts
@@ -31,7 +31,15 @@ let myLocalStorage = null as any as (typeof window.localStorage);
 try{
   myLocalStorage = window.localStorage;
 }catch(e) {
-  console.error(e);
+  // console.error(e);
+
+  window.top.postMessage({
+    type: 'error',
+    errorCode: '601',
+    content: e,
+    message: '当前可能处于无痕模式, 非同源下的 iframe 无法使用 cookie 和 localStorage, 可能将导致某些功能, 无法使用,建议点击全屏编辑.'
+  }, '*');
+
   myLocalStorage = (window as any).myLocalStorage || new MyLocalStorage();
   (window as any).myLocalStorage = myLocalStorage;
 }
